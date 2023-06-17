be620f58326e2805857052b37a49b7d3041406a4
argojkee <maksymhaidabura@gmail.com>
2023-06-17 17:00:41 +0300

    advantages

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,28 +1,51 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    const modals = [
+      'data-first-modal',
+      'data-second-modal',
+      'data-third-modal',
+    ];
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+    modals.forEach(element => {
+      const openModalSelector = element + '-open';
+      const closeModalSelector = element + '-close';
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
+      const openModalBtns = document.querySelectorAll(`[${openModalSelector}]`);
+      const closeModalBtns = document.querySelectorAll(
+        `[${closeModalSelector}]`
+      );
+      const modal = document.querySelector(`[${element}]`);
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+      if (!modal) logModalError('Can`t find Modal with attribute ' + modal);
+      if (openModalBtns.length === 0)
+        logModalError(
+          'Can`t find Open modal button with attribute ' + openModalSelector
+        );
+      if (closeModalBtns.length === 0)
+        logModalError(
+          'Can`t find Close modal button with attribute ' + closeModalSelector
+        );
+      if (!modal || openModalBtns.length === 0 || closeModalBtns.length === 0)
+        return;
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+      openModalBtns.forEach(openBtn =>
+        openBtn.addEventListener('click', toggleModal)
+      );
+      closeModalBtns.forEach(closeBtn =>
+        closeBtn.addEventListener('click', toggleModal)
+      );
+
+      function toggleModal() {
+        document.body.classList.toggle('modal-open');
+        modal.classList.toggle('is-hidden');
+      }
+    });
+  },
+  false
+);
+
+function logModalError(text) {
+  const styles = 'color: #bada55';
+  console.log('%c' + text, styles);
+}
